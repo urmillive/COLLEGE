f722b36d1d32e4e8e002633636ef02f4a30d4e4a
urmil89 <urmil8989@gmail.com>
2021-06-11 15:01:10 +0530

    adding files

---
+++ PHP/variables.php
@@ -0,0 +1,11 @@
+<?php
+/* in php we use $ sign for create variables
+there is no need of declare datatypes here php automatically do it for you
+
+*/
+
+$name = "Urmil";
+$age = 20;
+echo $name." is ".$age." years old";
+
+?>
